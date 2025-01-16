2a2860a869731b059ab6d24801a4a949a3f92135
JosephDSullivan <JosephDanielSullivan@GMail.com>
2025-01-16 14:26:55 -0500

    Playing in the sandbox.

--- src/sandbox.py
+++ src/sandbox.py
@@ -12,21 +12,31 @@ import sympy as sp
 #   Variable(s)
 #   None
 
-def main():
-    print('\n')
+def  main():
+    play_sympy()
 
-    x = sp.symbols('x')
-    f = sp.sin(x)
+    print("\nVariable List:")
+    print("globals")
+    for name, value in list(globals().items()):
+        print(f"\t{name} = {value}")
+    print("locals")
+    for name, value in list(locals().items()):
+        print(f"\t{name} = {value}")
+#
 
-    print('f(x)       = ' + str(f) + '\n')
+def play_sympy():
+    x = sp.symbols("x")
+    f = "sin(2*x)"
+
+    print("f(x)       = " + str(f) + "\n")
 
     f_diff = sp.diff(f, x)
 
-    print('derivative = ' + str(f_diff))
+    print("derivative = " + str(f_diff))
 
     f_integrate = sp.integrate(f, x)
 
-    print('integrate = ' + str(f_integrate))
+    print("integrate  = " + str(f_integrate))
 #
 
 if __name__ == "__main__":
