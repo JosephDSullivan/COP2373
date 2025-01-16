907e0e001df5e758e651d73358418b75bd62be98
JosephDSullivan <JosephDanielSullivan@GMail.com>
2025-01-16 14:26:36 -0500

    Create template for new assignment files.

---
+++ src/jdsullivan_chapterX_assignmentY.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#   COP2373 - Chapter  - Assignment .
+#
+#   tbd.
+#
+
+#   Import(s)
+#   None
+
+#   Variable(s)
+#   None
+
+def  main():
+    pass
+#
+
+if __name__ == "__main__":
+    #   Execute this code when invoked directly.
+    main()
+else:
+    #   Execute this code when imported.
+    pass
+#
