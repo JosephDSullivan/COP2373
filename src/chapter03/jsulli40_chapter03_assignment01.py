65a8c50ab1f530f7c747cc335794aa92016a45fe
JosephDSullivan <JosephDanielSullivan@GMail.com>
2025-02-07 14:51:51 -0500

    Initialize Chapter 03, Assignment 01.

---
+++ src/chapter03/jsulli40_chapter03_assignment01.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+""" This program is designed to pre-sell a limited number of cinema tickets.
+
+Author
+    Joseph D Sullivan <JSulli40@Student.SCF.edu>
+
+Date
+    February 7, 2025
+
+Chapter
+    03
+
+Assignment
+    01
+
+Repository
+    https://github.com/JosephDSullivan/COP2373/blob/main/src/chapter03/jsulli40_chapter03_assignment01.py
+
+Problem Description
+    Spam (or junk email) costs U.S. organizations billions of dollars a year
+    in spam-prevention software, equipment, network resources, bandwidth, and
+    lost productivity. Research online some of the most common spam email
+    messages and words. Create a list of 30 words and phrases commonly found
+    in spam messages. Write an application in which the user enters an email
+    message. Then your application will scan the message for each of the 30
+    keywords or phrases. For each occurrence of one of these within the
+    message, add a point to the message's "spam score". Next, rate the
+    likelihood that the message is spam, based on the number of points
+    received. Display the user's spam score, the likelihood message that it is
+    spam, and the words/phrases which caused it to be spam.
+    You should have at least two functions, but you can have more.
+    You must also have a technical design document (refer to the Submitting
+    Assignments Module).
+    Submit both your .py file and .doc/.docx file in this assignment and these
+    files must also be in your repository.
+"""
+
+#   Import(s)
+
+#   Constant(s)
+
+#   Variable(s)
+
+
+def main() -> None:
+    """
+    Entry function for when code is invoked directly.
+
+    Args:
+
+
+    Returns:
+        None
+    """
+    #   Run a sale of 20 total tickets with a maximum of 4 tickets per
+    #   transaction.
+    pass
+
+
+
+
+
+
+if __name__ == "__main__":
+    #   Execute this code when invoked directly.
+    main()
+else:
+    #   Execute this code when imported.
+    pass
